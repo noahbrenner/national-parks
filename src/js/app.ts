61e35fb41b4f8ee2df0512193bf936b01dd70ece
Noah Brenner <noah@noahbrenner.com>
2018-11-22 23:10:09 -0800

    Implement Basic Knockout ViewModel

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+import ko from 'knockout';
+import stub from './stub';
+
+class ViewModel {
+    public parks: ko.ObservableArray<string>;
+    public parkTypes: ko.ObservableArray<string>;
+
+    constructor() {
+        this.parks = ko.observableArray(stub.getParks());
+        this.parkTypes = ko.observableArray(stub.getParkTypes());
+    }
+}
+
+ko.applyBindings(new ViewModel());
---
+++ src/js/stub.ts
@@ -0,0 +1,11 @@
+export default {
+    getParkTypes: () => [
+        'National Park',
+        'Campground'
+    ],
+    getParks: () => [
+        'Park Name One',
+        'Second Park Name',
+        'A Third Park'
+    ]
+};
